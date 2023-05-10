14e3f9d661db8b9fa3dc84b1649f87d26ca8180c
MarcP70 <marcpourias@yahoo.fr>
2023-05-10 07:57:11 -0700

    Create: the file get_opcode to check the opcode

---
+++ get_opcode.c
@@ -0,0 +1,24 @@
+#include "monty.h"
+
+/**
+ * get_opcode_func - function to chech if the opcode exist in the struct
+ * @opcode: the opcode to check
+ * Return: the pointer of the function  associated
+*/
+void (*get_opcode_func(char *opcode))(stack_t **, unsigned int)
+{
+	instruction_t instructions[] = {
+		{"push", _push},
+		{"pall", _pall},
+		/* Add more opcodes and functions here */
+		{NULL, NULL}
+	};
+	int i;
+
+	for (i = 0; instructions[i].opcode; i++)
+	{
+		if (strcmp(opcode, instructions[i].opcode) == 0)
+			return (instructions[i].f);
+	}
+	return (NULL);
+}
