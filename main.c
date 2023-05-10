84e6641645f3e048d12961e30dcd7aa395609e88
root <6281@holbertonstudents.com>
2023-05-10 08:02:54 -0700

    Add: file function main, task 0

---
+++ main.c
@@ -0,0 +1,52 @@
+#include "monty.h"
+
+/**
+ * main - Entry point for the Monty interpreter
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: EXIT_SUCCESS on success, EXIT_FAILURE on failure
+ */
+int main(int argc, char **argv)
+{
+	FILE *fp;
+	char *line = NULL;
+	size_t len = 0;
+	ssize_t read;
+	unsigned int line_number = 0;
+	char *opcode;
+	stack_t *stack = NULL;
+
+	if (argc != 2)
+	{
+		fprintf(stderr, "USAGE: monty file\n");
+		exit(EXIT_FAILURE);
+	}
+	fp = fopen(argv[1], "r");
+	if (fp == NULL)
+	{
+		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
+		exit(EXIT_FAILURE);
+	}
+	while ((read = getline(&line, &len, fp)) != -1)
+	{
+		line_number++;
+		opcode = strtok(line, " \t\n");
+		/* If the line is empty or a comment, continue to the next line */
+		if (opcode == NULL || *opcode == '#')
+			continue;
+		if (get_opcode_func(opcode) == NULL)
+		{
+			fprintf(stderr, "L%u: unknown instruction %s\n", line_number, opcode);
+			free_stack(stack);
+			free(line);
+			fclose(fp);
+			exit(EXIT_FAILURE);
+		}
+		get_opcode_func(opcode)(&stack, line_number);
+	}
+	free_stack(stack);
+	free(line);
+	fclose(fp);
+	exit(EXIT_SUCCESS);
+}
