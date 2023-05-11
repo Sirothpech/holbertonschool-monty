c823a3304a92bfe602e32bd231a4ff769fefc40b
root <6281@holbertonstudents.com>
2023-05-11 08:16:11 -0700

    Fix: replace the word add by the word sub

--- _sub.c
+++ _sub.c
@@ -11,7 +11,7 @@ void _sub(stack_t **stack, unsigned int line_number)
 
 	if (*stack == NULL || (*stack)->next == NULL)
 	{
-		fprintf(stderr, "L%u: can't add, stack too short\n", line_number);
+		fprintf(stderr, "L%u: can't sub, stack too short\n", line_number);
 		globalstatus = EXIT_FAILURE;
 		return;
 	}
