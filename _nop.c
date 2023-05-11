e9ef2e144179c98b44e99a4fabc69e5bb3307126
root <6281@holbertonstudents.com>
2023-05-11 05:32:44 -0700

    Add: the file contains the function nop

---
+++ _nop.c
@@ -0,0 +1,12 @@
+#include "monty.h"
+
+/**
+ * _nop - function doesn’t do anything.
+ * @stack: pointer to the stack
+ * @line_number: line number of the file being read
+ */
+void _nop(stack_t **stack, unsigned int line_number)
+{
+	(void)stack;
+	(void)line_number;
+}
