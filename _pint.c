bb32a515320656630d514b0db3f80d2a7ba76fbe
MarcP70 <marcpourias@yahoo.fr>
2023-05-11 03:10:23 -0700

    Fix: betty style, add tab befor return

--- _pint.c
+++ _pint.c
@@ -12,7 +12,7 @@ void _pint(stack_t **stack, unsigned int line_number)
 	{
 		fprintf(stderr, "L%d: can't pint, stack empty\n", line_number);
 		globalstatus = EXIT_FAILURE;
-	        return;
+		return;
 	}
 	printf("%u\n", (*stack)->n);
 }
